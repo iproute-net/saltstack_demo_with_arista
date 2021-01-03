be2d856610e3e2ea28fb23262a753aa355bb945d
khelil sator <ksator@arista.com>
2021-01-03 15:47:40 +0100

    m

---
+++ start_saltstack.py
@@ -0,0 +1,19 @@
+# This scrip starts the salt-master service and the salt-minion service
+# For  each device it starts a salt-proxy daemon
+
+import os
+import yaml
+
+print ('starting salt master')
+os.system('docker exec -it salt service salt-master restart')
+
+print ('starting salt minion')
+os.system('docker exec -it salt service salt-minion restart')
+
+f=open('pillar/top.sls', 'r')
+vars = yaml.load(f.read())
+
+for item in vars['base']:
+    print ('starting salt proxy for device ' +  item)
+    shell_cmd = 'docker exec -it salt salt-proxy -d --proxyid=' + item
+    os.system(shell_cmd)
