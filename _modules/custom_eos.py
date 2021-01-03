8aba9bab593513b7ddf5c0b62dfb0a2979203002
khelil sator <ksator@arista.com>
2021-01-03 15:04:34 +0100

    m

--- _modules/custom_eos.py
+++ _modules/custom_eos.py
@@ -1,7 +1,13 @@
 def version():
-    res = __salt__['napalm.pyeapi_run_commands']('show version')
-    return res[0]['version']
+    if __grains__['os'] == 'eos':
+        res = __salt__['napalm.pyeapi_run_commands']('show version')
+        return res[0]['version']
+    else:
+        return 'Not supported on this platform'
 
 def model():
-    res = __salt__['napalm.pyeapi_run_commands']('show version')
-    return res[0]['modelName']
+    if __grains__['os'] == 'eos':
+        res = __salt__['napalm.pyeapi_run_commands']('show version')
+        return res[0]['modelName']
+    else:
+        return 'Not supported on this platform'
